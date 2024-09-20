e295edd422dfc3939d45b86aa90a39f311296c93
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-09-19 22:25:29 -0300

    primeiro commit

---
+++ to-do-list-react/src/App.jsx
@@ -0,0 +1,12 @@
+import ToDoList from "./componentes/ToDoList.jsx"
+
+function App() {
+
+  return (
+    <>
+      <ToDoList/>
+    </>
+  )
+}
+
+export default App
---
+++ to-do-list-react/src/componentes/ToDoList.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+function ToDoList(){
+
+    const [tarefas, setTarefas] = useState([])
+    const [novaTarefa, setNovaTarefa] = useState("")
+
+    return(
+        <>
+        </>
+    )
+}
+export default ToDoList
