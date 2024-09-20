3a10965378b67d74a15c216e69ac010333b6752b
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-09-19 22:31:43 -0300

    criando funções

--- to-do-list-react/src/componentes/ToDoList.jsx
+++ to-do-list-react/src/componentes/ToDoList.jsx
@@ -5,6 +5,28 @@ function ToDoList(){
     const [tarefas, setTarefas] = useState([])
     const [novaTarefa, setNovaTarefa] = useState("")
 
+
+
+    function entradaInput() {
+
+    } 
+
+    function adicionarTarefa() {
+
+    }
+
+    function removerTarefa(index) {
+
+    }
+
+    function subirTarefa(index) {
+
+    }
+
+    function descerTarefa(){
+
+    }
+    
     return(
         <>
         </>
