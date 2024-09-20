2d179e5024ef8b46b271577295dc140b7a265196
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-09-19 23:22:50 -0300

    css

--- to-do-list-react/src/componentes/ToDoList.jsx
+++ to-do-list-react/src/componentes/ToDoList.jsx
@@ -2,13 +2,11 @@ import React, {useState} from 'react'
 
 function ToDoList(){
 
-    const [tarefas, setTarefas] = useState([])
+    const [tarefas, setTarefas] = useState(["Estudar", "Limpar quarto", "Fazer compras"])
     const [novaTarefa, setNovaTarefa] = useState("")
 
-
-
-    function entradaInput() {
-
+    function entradaInput(event) {
+        setNovaTarefa(event.target.value)
     } 
 
     function adicionarTarefa() {
@@ -29,6 +27,27 @@ function ToDoList(){
     
     return(
         <>
+            <div className="lista">
+                <h1>Lista de tarefas</h1>
+
+                <div>
+                    <input type="text" placeholder='Digite a tarefa' value={novaTarefa} onChange={entradaInput}/>
+                    <button className="botaoAdicionar" onClick={adicionarTarefa}>Adicionar</button>
+                </div>
+
+                <ol>
+                    {tarefas.map((tarefa, index) => 
+                    <li key={index}>
+                        <span className="texto">{tarefa}</span>
+                        <button className="botaoDeletar" onClick={() => removerTarefa(index)}>Deletar</button>
+                        <button className="botaoMover" onClick={() => subirTarefa(index)}>Subir tarefa</button>
+                        <button className="botaoMover" onClick={() => descerTarefa(index)}>Descer tarefa</button>
+                    </li>)}
+                </ol>
+
+            </div>
+
+            
         </>
     )
 }
